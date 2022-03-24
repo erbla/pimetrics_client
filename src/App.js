b3637756a861732efec56f8486ba9b6d3812cfd5
Eric Blais <er.blais@gmail.com>
2022-03-24 16:53:09 -0400

    cleaned up unneded files, removed repetition from data fetch code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 
 function App() {
 
   const[lastping, setLastPing]=useState('');
 
-  useEffect( () => {
-    fetch('/time')
-      .then(response => response.json())
-      .then(data => setLastPing(data.lastping));
-  }, []);
-
-  useEffect( () => {
-    const interval = setInterval(() => {
+  useEffect(
+    function getMetrics() {
       fetch('/time')
         .then(response => response.json())
-        .then(data => setLastPing(data.lastping));
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [])
+        .then(data => setLastPing(data.lastping))
+        .catch((error) => console.log("Error contacting server: %s", error));
+      setTimeout(getMetrics, 1000);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Last time the Raspberry Pi communicated with the server: {lastping}
         </p>
